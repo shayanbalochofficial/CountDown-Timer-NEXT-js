f0ca32f05db8b9f8d5f514a84eaf66fede0fcf4f
Shayan Baloch <shayanbalochofficial@gmail.com>
2024-09-08 15:23:49 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Countdown from "@/components/countdown";
+function HomePage() {
+  return (
+    <>
+      <Countdown />
+    </>
+  );
+}
+
+export default HomePage;
